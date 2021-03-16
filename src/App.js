7e2726265ec3e002319a4d82e91c056461f5bbbd
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-03-17 00:57:09 +0800

    fire auth basic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app"
+import firebaseConfig from './firebase.config';
+import "firebase/auth";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app(); // if already initialized, use that one
+}
+// firebase.initializeApp(firebaseConfig);
 function App() {
+  const [mainUser, setMainUser] = useState({})
+  console.log(mainUser);
+  //provider
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  const FcebookProvider = new firebase.auth.FacebookAuthProvider();
+
+  //handler 1
+  const handlerForGoogle = () => {
+    firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        setMainUser(user)
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        // console.log(errorMessage);
+      });
+  }
+  //handler 2
+  const handlerForFacebook = () => {
+    firebase
+      .auth()
+      .signInWithPopup(FcebookProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        console.log(user, credential);
+      })
+      .catch((error) => {
+        var errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+  //handler 3
+  const githubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(githubProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+      }).catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorMessage);
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-5">
+      <button onClick={handlerForGoogle} className="btn btn-primary">Sing With Google</button>
+      <br />
+      <button onClick={handlerForFacebook} className="btn btn-primary mt-1">Sing With Facebook</button>
+      <br />
+      <button className="btn btn-primary mt-1" onClick={githubSignIn}> Sign in with github</button>
+      <h4>Hi, {mainUser.displayName}, Happy to see you</h4>
+      <img src={mainUser.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFVsVUSFRQ0xUKbQKo_kO4of0z4ibRDeA",
+    authDomain: "fir-reacap-42.firebaseapp.com",
+    projectId: "fir-reacap-42",
+    storageBucket: "fir-reacap-42.appspot.com",
+    messagingSenderId: "778174383205",
+    appId: "1:778174383205:web:c643c9c5597601f85bf28c"
+  };
+  export default firebaseConfig;
